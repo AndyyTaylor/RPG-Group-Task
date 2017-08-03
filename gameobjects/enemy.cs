17e15942ff3c3ed2596708355a4845f4aaa8be98
lachlanalcott04 <lachlan.alcott@student.tsc.nsw.edu.au>
2017-08-03 20:19:01 +1000

    FIX CAPTAIN!!!

--- gameobjects/enemy.cs
+++ gameobjects/enemy.cs
@@ -7,6 +7,9 @@ public class Enemy : GameObject {
     public Enemy(int _x, int _y, int _w, int _h) : base(_x, _y, _w, _h) {
 
     }
+public override void update() {
+
+}
 
 override void render(List<RenderObject> renderQueue) {
         renderQueue.Add(new RenderRect(x, y, w, h, Color.Black));
--- main.cs
+++ main.cs
@@ -8,52 +8,54 @@ public class MainClass {    // OOP ftw
     EventHandler eventHandler;
     Map gameMap;
     Player player;
+    Enemy Enemy;
     static Stopwatch stopWatch;
-    
+
     bool closed = false;
-    
+
     public MainClass() {
         mainForm = new MainForm(this);
         gameMap = new Map();
         player = new Player(1, 1, Map.SCALE, Map.SCALE);
-        
+
         eventHandler = new EventHandler(mainForm, player);
     }
-    
+
     public void update() {  /* Performs code logic */
         player.update();
     }
-    
+
     public void render() {  /* Adds all rendering to mainForm.renderQueue */
         gameMap.render(mainForm.getRenderQueue());
         player.render(mainForm.getRenderQueue());
-        
+        enemy.render(mainForm.getRenderQueue());
+
         mainForm.Refresh();
     }
-    
+
     public void exit(Object sender, FormClosingEventArgs e) {  /* When window is closed */
         closed = true;
     }
-    
+
     public bool checkClosed() {
         return closed;
     }
-    
+
     static public void Main () {  /* Main game loop */
         MainClass mainClass = new MainClass();
         stopWatch = new Stopwatch();
-        
+
         while (!mainClass.checkClosed()) {
             stopWatch.Start();
             mainClass.update();
             mainClass.render();
-            
+
             Application.DoEvents();
             System.Console.WriteLine(stopWatch.Elapsed);
             stopWatch.Restart();
-            
+
         }
-        
+
         System.Console.WriteLine("Exiting...");
         Application.Exit();
     }
