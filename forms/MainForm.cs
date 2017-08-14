37db3261ad61ba1559b22489dfd16ee199ce609b
lachlanalcott04 <lachlan.alcott@student.tsc.nsw.edu.au>
2017-08-14 10:32:51 +1000

    Bit of a menu bar

--- forms/MainForm.cs
+++ forms/MainForm.cs
@@ -7,15 +7,24 @@ public class MainForm : Form {  /* Main display window */
     private List<RenderObject> renderQueue = new List<RenderObject>();
     private float X_OFFSET, Y_OFFSET;
     Label label;
+    PictureBox picturebox;
 
     public MainForm (MainClass mainClass) {
         Text = "RPG Game";
         Size = new Size(900, 700);
         DoubleBuffered  = true;
 
+        picturebox = new PictureBox();
+        picturebox.Location = new Point(650,0);
+        picturebox.Text = "Abilities:";
+        picturebox.Size = new Size(400,800);
+        picturebox.Visible = true;
+        picturebox.BackColor = Color.Red;
+        Controls.Add(picturebox);
+
         label = new Label();
         label.Location = new Point(700, 25);
-        label.Text = "SUK MAD DECK";
+        label.Text = "Abilities:";
         label.Visible = true;
         Controls.Add(label);
 
@@ -43,6 +52,7 @@ public class MainForm : Form {  /* Main display window */
         }
 
         renderQueue.Clear();
+
     }
 
     public List<RenderObject> getRenderQueue() {
